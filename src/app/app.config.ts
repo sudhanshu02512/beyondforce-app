472df7519f68b9cbeefe1362631ce38d25dea9bf
Sudhanshu <sudhanshu02512@gmail.com>
2026-04-18 23:26:43 +0530

    KRS1804202601

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,11 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter, withHashLocation } from '@angular/router';
+import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes,withHashLocation()),
+    provideRouter(routes),
     provideHttpClient()
   ]
 };
